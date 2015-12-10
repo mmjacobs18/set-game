3f3079605194443b45592368a74b700b17d5cf15
Monica <mmjacobs18@gmail.com>
2015-12-09 19:20:27 -0500

    Added folder of card images and wrote methods to shuffle deck and check for a set

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+import copy
 from card import Card
+from random import shuffle
 
 colors=["red","green","purple"]
 shapes=["diamond","squiggle","elipse"]
 shades=["solid","shaded","clear"]
 
 def makeDeck(colors,shapes,shades):
+#creates and returns the complete deck of 81 cards 
     deck=[]
     for color in colors:
         for shape in shapes:
@@ -14,7 +17,37 @@ def makeDeck(colors,shapes,shades):
                     deck.append(Card(color,shape,shade,num))
     return deck
 
+def shuffleDeck(deck):
+#returns shuffled deck
+    shuffled_deck=copy.deepcopy(deck)
+    shuffle(shuffled_deck)
+    return shuffled_deck
 
+def isSet(card1,card2,card3):
+#returns True if Cards card1, card2, and card3 constitute a set
+    color= is_same(card1.color,card2.color,card3.color) or all_different(card1.color,card2.color,card3.color)
+    shape=is_same(card1.shape,card2.shape,card3.shape) or all_different(card1.shape,card2.shape,card3.shape)
+    shade=is_same(card1.shade,card2.shade,card3.shade) or all_different(card1.shade,card2.shade,card3.shade)
+    number=is_same(card1.num,card2.num,card3.num) or all_different(card1.num,card2.num,card3.num)
+    return color and shape and shade and number
+
+def is_same(item1,item2,item3):
+#returns True if item1, item2, and item3 are the same
+    if item1==item2 and item2==item3:
+        return True
+    else:
+        return False
+
+def all_different(item1,item2,item3):
+#returns True if item1, item2, and item3 are all different
+    if (item1!=item2 and item1!=item3) and item2!=item3:
+        return True
+    else:
+        return False
+
+
+#############################################################################
 if __name__=='__main__':
     deck=makeDeck(colors,shapes,shades)
-    print len(deck)
+    sd=shuffleDeck(deck)
+#    print isSet(deck[2],deck[5],deck[3])
